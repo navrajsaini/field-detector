5d5dc1d96cff1e580ce8dda9b743d2fe7f600812
Navraj Saini <navrajs.saini@gmail.com>
2022-11-20 08:39:42 -0700

    added in GDAL requirements info

--- field_detector.py
+++ field_detector.py
@@ -17,6 +17,14 @@ Return:
     location with '_ndvi_in_4326' in the name of the file
 
     An assumption is made here: The extention of the og file is '.tif'
+
+requirements:
+    GDAL 3.4.1, released 2021/12/27 installed
+    the venv already has the library installed
+
+- Active the venv
+- run field_detector.py with 'path' and 'threshold' in the args.
+- ex: 'python field_detector.py /path/to/.tiff 0.7'
 '''
 
 import sys
